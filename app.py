d3a7d4fc387060734724acdf3dbb1e2a4b3a0ea8
JackHo <514522970@qq.com>
2018-04-04 21:36:20 +0800

    orm2

--- app.py
+++ app.py
@@ -63,6 +63,31 @@ class  User(Model)
 	name = StringField()
 
 class Model(dict, metaclass = ModelMetaclass)
+	def __init__(self, **kw):
+		super(Model, self).__init__(**kw)
+
+	def __getattr__(self, key)
+		try:
+			return self[key]
+		except KeyError:
+			raise AttributeError(r"'model' object has no attribute '%s'"%key)
+			def __setattr(self, key, value):
+				self[key] = value
+
+			def getValue(self, key):
+				return getattr(self, key, None)
+
+			def getValueOrDefault(self, key):
+				value = getattr(self, key, None)
+				if value is None:
+					field = self.__mappings__[key]
+					if field.default is not None:
+						value = field.default() if callable(field.default) else field.default
+						logging.debug('using default value for %s:%s'%(key,str(value)))
+						setattr(self, key, value)
+						return value
+				pass
+				pass
 loop = asyncio.get_event_loop()
 loop.run_until_complete(init(loop))
 loop.run_forever()
